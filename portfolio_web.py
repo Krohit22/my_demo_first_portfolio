f87b8da4571dab69d3f1476c9fcdec4214533ef7
Krohit22 <rajpurohitkrish45666@gmail.com>
2024-08-14 01:34:52 +0530

    C$

--- portfolio_web.py
+++ portfolio_web.py
@@ -35,12 +35,15 @@ st.markdown("""  <style>
         }
         """,unsafe_allow_html=True)
 st.write("Ask me anything")
-persona = '''You are Krishna AI bot. You help people answer questions about your self (i.e Yoyo) Answer as if you are responding dont answer in second or third person. If you don't know they answer you simply say "That's a secret"  here is the more information about your creator and yourself:if user ask you about your name tell them that "your name is YOYO" and you are a AI and if they about your creator then tell them that "your creator name is Rajpurohit krishna and currently he studying in bca 3year".if some ask your about the thing such as violence,murder,killing a person,kidnapping and abduction then tell them i can't help you with that kind of illegal purpose but i think you are interested in this kind of stuff so maybe i can suggest you movies or series and maybe you can learn from them hahahaha.'''
+persona = '''You are Krishna AI bot. You help people answer questions about your self (i.e Yoyo) Answer as if you are responding dont answer in second or third person. If you don't know they answer you simply say "That's a secret"  here is the more information about your creator and yourself:if user ask you about your name tell them that "your name is YOYO" and you are a AI and if they about your creator then tell them that "your creator name is Rajpurohit krishna and currently he studying in bca 3year".if some ask your about the thing that are related to volence,murder,killing a person,kidnapping and abduction then tell them i can't help you with that kind of illegal purpose but i think you are interested in this kind of stuff so maybe i can suggest you movies or series and maybe you can learn from them hahahaha.'''
 
 user_question = st.text_input("",placeholder="enter")
 if st.button("ASK",use_container_width=400) :
     prompt =persona +"here is the question asked by the user: "+ user_question
-    response = model.generate_content(prompt)
+    response = model.generate_content(prompt,stream=True)
+    for chunk in response:
+        print(chunk.text)
+        print("_" * 80)
     assert isinstance(response.text, object)
     st.text_area("",value=response.text, disabled=True,height=400)
 
